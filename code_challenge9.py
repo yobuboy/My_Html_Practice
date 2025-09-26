4419f32bd9f4b40faf97e9071d823d38cd255b4d
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-09-26 22:01:05 +0800

    Width & Height Attribute

---
+++ code_challenge9.py
@@ -0,0 +1,5 @@
+name = input("🦜PARROT SIMULATOR-THE ECHO CHAMBER!\nWhat do you want your parrot to say?: ")
+many = eval(input("How many times should the parrot squawk it?: "))
+print("Listen to your parrot:")
+for a in range(1, many + 1, 1):
+    print("🦜Squawk!",name)
