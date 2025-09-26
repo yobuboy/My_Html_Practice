353144584093531fa228bacfe0f5a1699bf0635f
Alba, Mark Andrei J. <albamarkandrei09@gmail.com>
2025-09-26 22:08:56 +0800

    Delete code_challenge9.py

--- code_challenge9.py
+++
@@ -1,5 +0,0 @@
-name = input("🦜PARROT SIMULATOR-THE ECHO CHAMBER!\nWhat do you want your parrot to say?: ")
-many = eval(input("How many times should the parrot squawk it?: "))
-print("Listen to your parrot:")
-for a in range(1, many + 1, 1):
-    print("🦜Squawk!",name)
